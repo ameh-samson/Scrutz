efc0fa7ac18ba47c169487cb903cff9f0300f167
ameh-samson <65107918+ameh-samson@users.noreply.github.com>
2024-06-16 17:46:34 +0100

    install shadcdn ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/ui/Layout";
+
 function App() {
-  return <div className="bg-white font-nunito">App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/career" element={<Careers />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/signup" element={<SignupForm />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
