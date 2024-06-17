2a579dad88cef4c78a9c5662f07cb47bbed78d35
ameh-samson <65107918+ameh-samson@users.noreply.github.com>
2024-06-17 08:11:53 +0100

    Update Overview.jsx

--- src/pages/Overview.jsx
+++ src/pages/Overview.jsx
@@ -1,13 +1,23 @@
 import { DatePickerWithRange } from "@/components/ui/date-range-picker";
 import SectionTitle from "./custom/SectionTitle";
 import NoActivity from "./custom/NoActivity";
+import { Button } from "@/components/ui/button";
+import { CiExport } from "react-icons/ci";
 
 const Overview = () => {
   return (
     <div>
       <div className="flex justify-between items-center">
         <SectionTitle>Overview</SectionTitle>
-        <DatePickerWithRange />
+
+        <div className="flex items-center gap-4">
+          <DatePickerWithRange />
+
+          <Button>
+            <CiExport />
+            Export
+          </Button>
+        </div>
       </div>
 
       <div>
