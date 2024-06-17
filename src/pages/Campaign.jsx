91ed14ccf9b916aa23ae090f6c750eaf8fe9fe70
ameh-samson <65107918+ameh-samson@users.noreply.github.com>
2024-06-17 08:55:35 +0100

    Update Campaign.jsx

--- src/pages/Campaign.jsx
+++ src/pages/Campaign.jsx
@@ -9,20 +9,39 @@ const Campaign = () => {
       <div>
         <SectionTitle>All Campaigns</SectionTitle>
       </div>
-      <div>
-        <span>All(90)</span>
-        <span>Inactive(90)</span>
-        <span>Active(90)</span>
-      </div>
 
-      <div className="relative w-60">
-        <Input type="text" placeholder="  Search..." className="w-full pl-10" />
-        <IoIosSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
-      </div>
+      <div className="flex items-center justify-between mt-6">
+        <div className="flex gap-4">
+          <span className="p-3 border border-darkCyan rounded text-sm text-darkCyan">
+            All(90)
+          </span>
+          <span className="p-3 border border-darkCyan rounded text-sm text-darkCyan">
+            Inactive(90)
+          </span>
+          <span className="p-3 border border-darkCyan rounded text-sm text-darkCyan">
+            Active(90)
+          </span>
+        </div>
 
-      <div className="relative w-60">
-        <Input type="text" placeholder="  Search..." className="w-full pl-10" />
-        <FaChevronDown className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+        <div className="flex items-center gap-7">
+          <div className="relative w-60">
+            <Input
+              type="text"
+              placeholder="  Search..."
+              className="w-full pl-10"
+            />
+            <IoIosSearch className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+          </div>
+
+          <div className="relative w-60">
+            <Input
+              type="text"
+              placeholder="  Filter by date..."
+              className="w-full pl-10"
+            />
+            <FaChevronDown className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400" />
+          </div>
+        </div>
       </div>
     </div>
   );
