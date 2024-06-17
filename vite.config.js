b6e050a4597e7a26ddaee38e0e9de5d6f473af90
Samson Ameh <65107918+ameh-samson@users.noreply.github.com>
2024-06-17 19:05:32 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,11 +5,13 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
-  // for shadcdn component
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1000, // Adjust the limit as needed
+  },
 });
+
