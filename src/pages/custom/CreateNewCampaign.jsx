2acbba79384f1d7870e7f4b310d7a9ef2c57a337
Samson Ameh <65107918+ameh-samson@users.noreply.github.com>
2024-06-18 20:10:42 +0100

    remove form

--- src/pages/custom/CreateNewCampaign.jsx
+++ src/pages/custom/CreateNewCampaign.jsx
@@ -3,12 +3,10 @@ import SectionTitle from "./SectionTitle";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { newCampaignFormSchema } from "@/formsValidation";
-import { useGlobalContext } from "@/context/Context";
+
 import CreateNewForm from "./Form";
 
 const CreateNewCampaign = () => {
-  const { newCampaign, setNewCampaign } = useGlobalContext();
-
   const form = useForm({
     resolver: zodResolver(newCampaignFormSchema),
     defaultValues: {
